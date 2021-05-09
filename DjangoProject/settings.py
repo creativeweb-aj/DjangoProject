ee482fb96383fdd0c952b09a52efd9fd575a0333
ajay <webcreations100@gmail.com>
2021-05-09 09:31:04 +0000

    update from beta server

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-from DjangoProject.local.dev import *
 #from DjangoProject.local.dev import *
+from DjangoProject.prod.prod import *
 
 # Application definition
 
