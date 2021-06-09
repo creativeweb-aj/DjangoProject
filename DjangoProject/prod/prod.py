3ef9339198ed22113be2deae7392b76098e2b288
Ajay kumar sharma <48996332+creativeweb-aj@users.noreply.github.com>
2021-06-09 17:52:08 +0530

    database option set

--- DjangoProject/prod/prod.py
+++ DjangoProject/prod/prod.py
@@ -21,6 +21,9 @@ DATABASES = {
         'USER': os.environ.get("DATABASE_USERNAME"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
         'HOST': os.environ.get('DATABASE_HOST'),
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
